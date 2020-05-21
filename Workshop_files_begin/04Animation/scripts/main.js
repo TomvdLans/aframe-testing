29d61acd4a61bfd4647d83517106a8d9919213dc
TomvdLans <37619190+TomvdLans@users.noreply.github.com>
2020-05-21 17:40:52 +0200

    Update main.js

--- Workshop_files_begin/04Animation/scripts/main.js
+++ Workshop_files_begin/04Animation/scripts/main.js
@@ -9,4 +9,14 @@ function spin(){
   console.log(myOtherBox.object3D.rotation.x);
 }
 
+function spin(){
+  myOtherBox.object3D.rotation.y += rotationSpeed;
+  console.log(myOtherBox.object3D.rotation.y);
+}
+
+function spin(){
+  myOtherBox.object3D.rotation.z += rotationSpeed;
+  console.log(myOtherBox.object3D.rotation.z);
+}
+
 setInterval(spin, 16); //equivalent to 60 fps
