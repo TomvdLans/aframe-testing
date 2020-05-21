a24c3511789fbf39ce93c673775a0714f77f5da8
tomvdl <tomvdl205@gmail.com>
2020-05-21 20:38:58 +0200

    animation slow down

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,13 +2,18 @@
 
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.05;
+var rotationSlowDown = -0.0005
 var myOtherBox = document.getElementById('myOtherBox');
+
 
 function spin(){
   myOtherBox.object3D.rotation.x += rotationSpeed;
   myOtherBox.object3D.rotation.y += rotationSpeed;
   myOtherBox.object3D.rotation.z += rotationSpeed;
-  console.log(myOtherBox.object3D.rotation.x);
+
+  if (rotationSpeed >= 0.001 ) {
+    rotationSpeed += rotationSlowDown;
+  }
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
